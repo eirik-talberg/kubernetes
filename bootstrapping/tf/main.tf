27ef9d3269db1581316e138a16b3ac4dc3d0a37a
Eirik W. Talberg <eirikt@netcompany.com>
2022-12-03 21:29:10 +0100

    psql helm params

--- bootstrapping/tf/main.tf
+++ bootstrapping/tf/main.tf
@@ -82,68 +82,113 @@ resource "kubernetes_manifest" "mgmt-apps" {
   }
 }
 
-/**
-resource "kubernetes_manifest" "common-apps" {
+provider "kubernetes" {
+  alias = "workload"
+  config_path = "~/.kube/config-k3s-workload"
+}
+
+resource "kubernetes_service_account" "argo-cd" {
+  provider = kubernetes.workload
+  metadata {
+    name = "argo-cd"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argo-cd" {
+  provider = kubernetes.workload
+  metadata {
+    name = "argo-cd"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "argo-cd"
+    namespace = "default"
+  }
+}
+
+resource "kubernetes_manifest" "argo-cd-secret" {
+  provider = kubernetes.workload
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Secret"
+    metadata = {
+      "name" = "argo-cd-sa-secret"
+      "namespace" = "default"
+      "annotations" = {
+        "kubernetes.io/service-account.name" = "argo-cd"
+      }
+    }
+    "type" = "kubernetes.io/service-account-token"
+  }
+}
+
+data "kubernetes_secret" "argo-cd-token-secret" {
+  provider = kubernetes.workload
+  metadata {
+    name = "argo-cd-sa-secret"
+    namespace = "default"
+  }
+}
+
+resource "kubernetes_secret" "workload-cluster" {
+  metadata {
+    name = "workload-cluster"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "cluster"
+    }
+  }
+  data = {
+    "name" = "k3s-workload"
+    "server" = "https://10.0.69.14:6443"
+    "config" =jsonencode( {
+      "bearerToken" = "${data.kubernetes_secret.argo-cd-token-secret.data.token}"
+      "tlsClientConfig" = {
+        "insecure": false,
+        "caData": base64encode("${data.kubernetes_secret.argo-cd-token-secret.data["ca.crt"]}")
+      }
+    })
+  }
+}
+
+resource "kubernetes_namespace" "namespaces" {
+  provider = kubernetes.workload
+  for_each = toset(["media", "prod", "staging", "dev"])
+  metadata {
+    name = each.value
+  }
+}
+
+resource "kubernetes_manifest" "workload-apps" {
   manifest = {
     "apiVersion" ="argoproj.io/v1alpha1"
-    "kind" = "ApplicationSet"
+    "kind" = "Application"
     metadata = {
-      "name" = "common-apps"
+      "name" = "workload-apps"
       "namespace" = "argocd"
+      "finalizers" = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
     }
     "spec" = {
-      "generators" = [
-          {
-            "matrix" = {
-              "generators" = [
-                  {
-                    "git" = {
-                      "repoURL" = "git@github.com:eirik-talberg/kubernetes"
-                      "revision" = "HEAD"
-                      "directories" = [
-                        {
-                          "path" = "bootstrapping/argo-cd/common-apps/*"
-                        }
-                      ]
-                    }
-                  },
-                  {
-                    "list"= {
-                      "elements" = [
-                        {
-                          "cluster" = "mgmt"
-                          "url" = "https://kubernetes.default.svc"
-                          "values" = {
-                            "project" = "default"
-                          }
-                        }
-                      ]
-                    }
-                  }
-                ]
-              
-            }
-          }
-        ]
-      
-      "template" = {
-        "metadata" = {
-          "name" = "{{ .path.basename }}-{{ .name }}"
-        }
-        "spec" = {
-          "project" = "default"
-          "source" = {
-            "repoURL" = "git@github.com:eirik-talberg/kubernetes"
-            "targetRevision" = "HEAD"
-            "path" = "{{ .path.path }}"
-          }
-          "destination" = {
-            "server" = "{{ .url }}"
-            "namespace" = "{{ .path.basename }}"
-          }
-        }
+      "destination" = {
+        "namespace" = "argocd"
+        "server": "https://kubernetes.default.svc"
+      }
+      "project" = "default"
+      "source" = {
+        "path" = "bootstrapping/argo-cd/clusters/workload"
+        "repoURL" = "git@github.com:eirik-talberg/kubernetes"
+        "targetRevision": "HEAD"
+      }
+      "syncPolicy" = {
+        "automated" = {}
       }
     }
   }
 }
-*/
